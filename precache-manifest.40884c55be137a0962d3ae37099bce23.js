9d892f0dd056c019ebbc17dc4e1e8290885ad46a
koylyakandriy <koylyakandriy@gmail.com>
2019-09-24 11:45:44 +0300

    Updates

---
+++ precache-manifest.40884c55be137a0962d3ae37099bce23.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636a23c2e31b50384e9680bc4dd762ec",
+    "url": "/creativeAgency/index.html"
+  },
+  {
+    "revision": "052a335086d45410682d",
+    "url": "/creativeAgency/static/css/main.58f87762.chunk.css"
+  },
+  {
+    "revision": "2a5ea7034e417420082b",
+    "url": "/creativeAgency/static/js/2.1741a26a.chunk.js"
+  },
+  {
+    "revision": "052a335086d45410682d",
+    "url": "/creativeAgency/static/js/main.831f8b10.chunk.js"
+  },
+  {
+    "revision": "1707253947a2dad1dcdc",
+    "url": "/creativeAgency/static/js/runtime~main.9921e45f.js"
+  },
+  {
+    "revision": "56cf59e591dab96dd3c50347597f8a00",
+    "url": "/creativeAgency/static/media/background.56cf59e5.jpeg"
+  },
+  {
+    "revision": "166f22a94fed8619a5665fe3a4e1d25f",
+    "url": "/creativeAgency/static/media/background_2.166f22a9.jpg"
+  },
+  {
+    "revision": "740fc8176b3a42f53d948a3894920c87",
+    "url": "/creativeAgency/static/media/boy.740fc817.png"
+  },
+  {
+    "revision": "74da07214279d76f9809b9c3943f23e6",
+    "url": "/creativeAgency/static/media/girl.74da0721.png"
+  },
+  {
+    "revision": "6a5cafca25092ec51f0db68156023885",
+    "url": "/creativeAgency/static/media/images.6a5cafca.jpeg"
+  },
+  {
+    "revision": "7dfe6cc41c1fb47800c69628178dc729",
+    "url": "/creativeAgency/static/media/man (1).7dfe6cc4.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/creativeAgency/static/media/man.bd80794f.png"
+  }
+]);
